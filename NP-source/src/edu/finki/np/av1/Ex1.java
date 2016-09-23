b2e1b3106a1974b29a375b1198b748566c6b654e
Tomche Delev <tomce.delev@gmail.com>
2016-09-23 12:23:15 +0200

    Some fomatting and testing

--- NP-source/src/edu/finki/np/av1/Ex1.java
+++ NP-source/src/edu/finki/np/av1/Ex1.java
@@ -1,10 +1,23 @@
 package edu.finki.np.av1;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Ex1 {
 	public static void main(String[] args) {
-		for(int a = 0; a < 1000; a++) {
-			for(int b = a + 1; b < 1000; b++) {
-				if((a * a + b * b + 1) % (a + b) == 0) {
+		IntStream.range(0, 10)
+		.forEach(x -> System.out.println(x));
+		
+		for(int i = 0; i < 10; ++i) {
+			System.out.println(i);
+		}
+		
+		
+		
+		for (int a = 0; a < 1000; a++) {
+			for (int b = a + 1; b < 1000; b++) {
+				if ((a * a + b * b + 1) % (a + b) == 0) {
 					System.out.println(String.format("%d %d", a, b));
 				}
 			}
