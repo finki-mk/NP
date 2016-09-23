206f5b33791c5307d13c44ba8cfb9c8db332ada7
Tomche Delev <tomce.delev@gmail.com>
2016-09-23 15:22:46 +0200

    Homework setup for av1

---
+++ examples/src/main/java/mk/ukim/finki/np/av1/NumberPrinter.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.np.av1;
+
+/**
+ * Homework 1.2 number printer
+ */
+public class NumberPrinter {
+
+    /**
+     * Print numbers from 1 to howMany in following format [1][2]...[howMany]
+     *
+     * @param howMany    how many numbers to print
+     * @param lineLength the length of the lin
+     */
+    public static void formatNumbers(int howMany, int lineLength) {
+        // your solution here
+    }
+
+    public static void main(String[] args) {
+        formatNumbers(250, 60);
+    }
+}
---
+++ examples/src/main/java/mk/ukim/finki/np/av1/RomanConverter.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.np.av1;
+
+/**
+ * Homework 1.1 Roman converter
+ */
+public class RomanConverter {
+    /**
+     * Roman to decimal converter
+     *
+     * @param n number in decimal format
+     * @return string representation of the number in Roman numeral
+     */
+    public static String toRoman(int n) {
+        // your solution here
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RomanConverter.toRoman(1998));
+    }
+}
